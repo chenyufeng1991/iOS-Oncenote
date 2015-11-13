2a84896890c03b8d0af66dee5a99b57c864f1011
chenyufeng <503434355@qq.com>
2015-11-13 20:37:20 +0800

    dev:完成登录注册操作，还需要使用官方API来优化注册操作

--- Oncenote/RegisterViewController.m
+++ Oncenote/RegisterViewController.m
@@ -7,28 +7,65 @@
 //
 
 #import "RegisterViewController.h"
+#import "BmobOperation.h"
 
 @interface RegisterViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *usernameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
 
 @end
 
 @implementation RegisterViewController
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
-  
-  
+  [super viewDidLoad];
   
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+#pragma mark - 所有按钮的点击操作；
+- (IBAction)createAccountButtonPressed:(id)sender {
+  NSString *username = [self.usernameTextField.text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+  NSString *password = [self.passwordTextField.text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+  
+  if (![username  isEqual: @""] && ![password  isEqual: @""]) {
+    //用户名密码同时不为空，才可以进行注册；
+    BmobOperation *addUser = [[BmobOperation alloc] init];
+    [addUser addUserToUserTable:@"_User" username:username password:password todo:^(BOOL isSuccessful, NSError *error) {
+      //todo;
+      if (isSuccessful) {
+        [self showPromptDialog:@"提示" andMessage:@"创建账户成功，请登录！" andButton:@"确定" andAction:^(UIAlertAction *action) {
+          
+        }];
+      } else {
+        NSLog(@"注册失败：%@",error);
+      }
+    }];
+    
+  }else{
+    [self showPromptDialog:@"提示" andMessage:@"用户名密码不能为空！" andButton:@"确定" andAction:^(UIAlertAction *action) {
+      
+    }];
+  }
+  
 }
-*/
+
+#pragma mark - 触摸屏幕隐藏键盘
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+  
+  [self.usernameTextField resignFirstResponder];
+  [self.passwordTextField resignFirstResponder];
+  
+}
+
+#pragma mark - 弹出提示对话框
+- (void)showPromptDialog:(NSString*)title andMessage:(NSString*)message andButton:(NSString*)buttonTitle andAction:(void (^ __nullable)(UIAlertAction *action)) handler{
+  
+  //尝试使用新的弹出对话框；
+  UIAlertController *alertController = [UIAlertController alertControllerWithTitle:title message:message preferredStyle:UIAlertControllerStyleAlert];
+  [alertController addAction:[UIAlertAction actionWithTitle:buttonTitle style:UIAlertActionStyleDefault handler:handler]];
+  //弹出提示框；
+  [self presentViewController:alertController animated:true completion:nil];
+}
 
 @end

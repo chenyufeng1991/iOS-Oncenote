b49b5369423c5735147b4e4ac3c803f266f7caf5
chenyufeng <503434355@qq.com>
2015-11-15 21:51:31 +0800

    dev:完成记住用户名密码的功能，用户不需要每次登录都输入密码，可以直接进入系统

---
+++ Oncenote/LaunchScreenViewController.m
@@ -0,0 +1,65 @@
+
+//
+//  LaunchScreenViewController.m
+//  Oncenote
+//
+//  Created by chenyufeng on 15/11/15.
+//  Copyright © 2015年 chenyufengweb. All rights reserved.
+//
+
+#import "LaunchScreenViewController.h"
+#import "AppDelegate.h"
+
+@interface LaunchScreenViewController ()
+
+@end
+
+@implementation LaunchScreenViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  
+
+  
+}
+
+
+- (void)viewDidAppear:(BOOL)animated{
+
+  [super viewDidAppear:animated];
+  NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+  
+  if ([userDefaults objectForKey:@"username"] != nil && [userDefaults objectForKey:@"password"] != nil ) {
+    //直接跳到主界面；
+    NSLog(@"直接跳到主界面");
+    
+    AppDelegate *globalApp = [[UIApplication sharedApplication] delegate];
+    globalApp.GLOBAL_USERNAME = [userDefaults objectForKey:@"username"];
+    globalApp.GLOBAL_USERID = [userDefaults objectForKey:@"userId"];
+    
+    
+    UIViewController *mainViewController = [[UIViewController alloc] init];
+    mainViewController = [self.storyboard instantiateViewControllerWithIdentifier:@"MainViewController"];
+    [self presentViewController:mainViewController animated:true completion:nil];
+    
+    
+  }else{
+    NSLog(@"跳到登录界面");
+    //跳到登录界面；
+    UIViewController *loginViewController = [[UIViewController alloc] init];
+    loginViewController = [self.storyboard instantiateViewControllerWithIdentifier:@"LoginViewController"];
+    [self presentViewController:loginViewController animated:true completion:nil];
+    
+    
+  }
+}
+
+
+@end
+
+
+
+
+
+
+
